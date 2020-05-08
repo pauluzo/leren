b9e73fd27153b5a23f2d1791fa62797b7b381211
pauluzo <45356331+pauluzo@users.noreply.github.com>
2020-05-08 12:56:24 +0100

    minor bugs fixed

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -271,10 +271,10 @@ const BackDrop = props => {
           if(user.details.email === email && user.details.password === password) {
             console.log("this runs");
             localStorage.setItem("userData", JSON.stringify(user));
-            if(user.isInstructor === true) {
+            if(user.isInstructor) {
               console.log("instructor");
               history.push("/profile", "instructor");
-            } else if(user.isStudent === true) {
+            } else if(user.isStudent) {
               console.log("student");
               history.push("/profile", "student");
             }
